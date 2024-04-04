15c9319bc7f6d93162ec9b49f600f76c5111f5a4
Viniciuslatini <vinicius.latini@estudante.ufjf.br>
2024-04-04 18:57:12 -0300

    feat: add create-user route

---
+++ src/http/routes/create-user.ts
@@ -0,0 +1,25 @@
+import { prisma } from "../../lib/prisma";
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+
+export async function createUser(app: FastifyInstance) {
+  app.post("/user", async (req, res) => {
+    const createUserBody = z.object({
+      name: z.string(),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password } = createUserBody.parse(req.body);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+
+    return res.status(201).send({ userId: user.id });
+  });
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,10 @@
 import fastify from "fastify";
 
+import { createUser } from "./routes/create-user";
+
 const app = fastify({ logger: true });
+
+app.register(createUser);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
