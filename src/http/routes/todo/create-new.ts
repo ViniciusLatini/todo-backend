6fa83404e16c0e0df4f491de7888371dafccda06
Viniciuslatini <vinicius.latini@estudante.ufjf.br>
2024-04-06 18:09:48 -0300

    feat: add createTodo

---
+++ src/http/routes/todo/create-new.ts
@@ -0,0 +1,31 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../../../lib/prisma";
+
+export async function createTodo(app: FastifyInstance) {
+  app.post("/todo/:userId", async (req, res) => {
+    try {
+      const { userId } = req.params as { userId: string };
+      const createTodoBody = z.object({
+        title: z.string(),
+      });
+
+      const { title } = createTodoBody.parse(req.body);
+
+      const todo = await prisma.todo.create({
+        data: {
+          title,
+          user: {
+            connect: { id: userId },
+          },
+          completed: false,
+        },
+      });
+
+      return res.status(201).send(todo);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({ error: "Internal server error" });
+    }
+  });
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -5,6 +5,7 @@ import { getUserById } from "./routes/user/get-by-id";
 import { deleteUser } from "./routes/user/delete";
 import { getAllUsers } from "./routes/user/get-all";
 import { updateUser } from "./routes/user/update";
+import { createTodo } from "./routes/todo/create-new";
 
 const app = fastify({ logger: true });
 
@@ -13,6 +14,8 @@ app.register(getUserById);
 app.register(getAllUsers);
 app.register(deleteUser);
 app.register(updateUser);
+
+app.register(createTodo);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
