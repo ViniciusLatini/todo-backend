f63cbb8f8b8023ba04cd03ae697cb8bd25b0dec2
Viniciuslatini <vinicius.latini@estudante.ufjf.br>
2024-04-06 17:35:59 -0300

    feat: add get all users route

---
+++ src/http/routes/user/get-all.ts
@@ -0,0 +1,15 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../../../lib/prisma";
+
+export async function getAllUsers(app: FastifyInstance) {
+  app.get('/users', async (req, res) => {
+    try {
+      const users = await prisma.user.findMany();
+
+      return res.status(200).send(users);
+    } catch (error) {
+      console.log(error)
+      return res.status(500).send({ error: "Internal server error" });
+    }
+  })
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -2,12 +2,15 @@ import fastify from "fastify";
 
 import { createUser } from "./routes/user/create-new";
 import { getUserById } from "./routes/user/get-by-id";
-import { deleteUser } from "./routes/user/delete"
+import { deleteUser } from "./routes/user/delete";
+import { getAllUsers } from "./routes/user/get-all";
+
 const app = fastify({ logger: true });
 
 app.register(createUser);
 app.register(getUserById);
-app.register(deleteUser)
+app.register(getAllUsers)
+app.register(deleteUser);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
